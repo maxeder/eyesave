7df33a825242ad19f8c90c72adb5704988171a90
Maximilian Eder <max.eder@aol.com>
2020-11-07 15:13:20 +0100

    Add timer and about popover

--- eyesave/AppDelegate.swift
+++ eyesave/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
     var statusBarItem: NSStatusItem!
+    var timer: Timer!
+    var popover: NSPopover!
 
 
 
@@ -24,7 +26,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusBarItem = statusBar.statusItem(
             withLength: NSStatusItem.squareLength)
         
-        //statusBarItem.button?.title = "👀"
         statusBarItem.button?.image = NSImage(named: "Icon")
         let statusBarMenu = NSMenu(title: "Cap Status Bar Menu")
         
@@ -39,6 +40,25 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             withTitle: "Cancel Timer",
             action: #selector(AppDelegate.cancelTimer),
             keyEquivalent: "")
+        
+        statusBarMenu.addItem(NSMenuItem.separator())
+        
+        statusBarMenu.addItem(
+            withTitle: "About",
+            action: #selector(togglePopover(_:)),
+            keyEquivalent: "")
+        
+        
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+        
+        // create popover
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 400, height: 400)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        self.popover = popover
+        
  
     }
     
@@ -47,18 +67,38 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @objc func startTimer() {
         print("Starting Timer")
         
+        timer = Timer.scheduledTimer(timeInterval: 1200, target: self, selector: #selector(sendNotification), userInfo: nil, repeats: true)
+        
+        //notification.deliveryDate = Date(timeIntervalSinceNow: 5)
+        //NSUserNotificationCenter.default.scheduleNotification(notification)
+    }
+
+    @objc func cancelTimer() {
+        print("Canceled Timer")
+        timer.invalidate()
+    }
+    
+    
+    @objc func togglePopover(_ sender: AnyObject?) {
+         if let button = self.statusBarItem.button {
+              if self.popover.isShown {
+                   self.popover.performClose(sender)
+              } else {
+                   self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+              }
+         }
+    }
+    
+    
+    
+    
+    @objc func sendNotification() {
         let notification = NSUserNotification()
             notification.title = "EyeSave"
             notification.subtitle = "Take a break!"
             notification.soundName = NSUserNotificationDefaultSoundName
             NSUserNotificationCenter.default.deliver(notification)
         
-        notification.deliveryDate = Date(timeIntervalSinceNow: 5)
-        NSUserNotificationCenter.default.scheduleNotification(notification)
-    }
-
-    @objc func cancelTimer() {
-        print("Canceled Timer")
     }
     
     
